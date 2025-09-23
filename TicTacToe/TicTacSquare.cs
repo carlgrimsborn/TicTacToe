4da48109ce6141af8a729406b638a86eb3188f3d
carlgrimsborn <carlgrimsborn@gmail.com>
2025-09-23 13:15:30 +0200

    square logic

---
+++ TicTacToe/TicTacSquare.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    enum VariantType
+    {
+        Cross,
+        Cirle
+    }
+
+    internal class TicTacSquare
+    {
+        public int Variant { get; set; }
+    }
+}
