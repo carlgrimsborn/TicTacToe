2923a550bb8ec8ebcfd2ee19965e2288c09811bd
Emilia <emilia.tasci7@gmail.com>
2025-09-28 18:14:43 +0200

    Test version med en klass och metod//emilia

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -5,10 +5,76 @@
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Heloooo;"); 
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\n\t\n\tVälkommen till TicTacToe!!!!");
+            Console.ReadKey();
+            Console.WriteLine("\n\t\n\tTvå spelare ska ska mata in tal 1-9 tills de får tre i rad.");
+            Console.ReadKey();
+            Console.WriteLine($"\n\t\n\t__1__ __2__ __3__"); //självaste board
+            Console.WriteLine($"\n\t\n\t__4__ __5__ __6__");
+            Console.WriteLine($"\n\t\n\t__7__ __8__ __9__");
 
+            string[] Board = { "1", "2", "3", "4", "5", "6", "7", "8", "9" }; //array för board
+            WinnerResultcs result = new WinnerResultcs();
+            int allaDrag = 0;
+            while (true)
+            {
+
+                Console.WriteLine("\n\t\n\tSpelare1 (X) Välj mellan 1-9");//fråga spelare1
+                string input = Console.ReadLine();
+                int indexplayer1 = int.Parse(input) - 1;
+                string player1 = "X";
+
+
+                if (Board[indexplayer1] != "X" && Board[indexplayer1] != "O")
+                {
+                    Board[indexplayer1] = player1;
+
+                    allaDrag++;
+
+                }
+                result.DidPlayerWin(allaDrag, Board);
+                WholeBoard(Board); //Board skall visas efter spelar1 tur
+
+                Console.WriteLine("\n\t\n\tSpelare2 (O) Välj mellan 1-9"); //fråga spelare2
+                string input2 = Console.ReadLine();
+                int indexplayer2 = int.Parse(input2) - 1;
+                string player2 = "O";
+
+
+                if (Board[indexplayer2] != "O" && Board[indexplayer2] != "X")
+                {
+
+                    Board[indexplayer2] = player2;
+                    allaDrag++;
+                    //Board skall visas efter spelar2 tur
+
+                }
+
+                WholeBoard(Board);
+                result.DidPlayerWin(allaDrag, Board);
+
+            }
+
+        }
+
+        static void WholeBoard(string[] Board)
+        {
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.WriteLine($"\n\t\n\t__{Board[0]}__ __{Board[1]}__ __{Board[2]}__"); //självaste board i en metod
+            Console.WriteLine($"\n\t\n\t__{Board[3]}__ __{Board[4]}__ __{Board[5]}__");
+            Console.WriteLine($"\n\t\n\t__{Board[6]}__ __{Board[7]}__ __{Board[8]}__");
 
 
         }
+
+
+
     }
+
+
+
 }
+    
---
+++ TicTacToe/WinnerResultcs.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    internal class WinnerResultcs
+    {
+
+        /// public int drag;
+        // public int räknedrag;
+        //public int Winner;
+        private string[] WinConditions { get; set; } = { "123", "456", "789", "741", "852", "963", "159", "753" }; //String[] med alla möjliga vinster (Från Thomas och Carl)
+
+        public void DidPlayerWin(int allaDrag, string[] Board)
+        {
+
+            if (allaDrag >= 5) //Börja inte kolla efter en vinst innan 5 drag har genomförts
+            {
+                foreach (string winCondition in WinConditions)
+                {
+                    if (Board[int.Parse(winCondition[0].ToString()) - 1] == "X" &&
+                        Board[int.Parse(winCondition[1].ToString()) - 1] == "X" &&
+                       Board[int.Parse(winCondition[2].ToString()) - 1] == "X")
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("\n\t\n\tSpelare1 har vunnit!!");
+                        Console.ForegroundColor = ConsoleColor.Gray;
+
+
+                        Environment.Exit(0);
+
+                        //Avsluta spelet
+
+
+
+                    }
+                    else if (Board[int.Parse(winCondition[0].ToString()) - 1] == "O" &&
+                        Board[int.Parse(winCondition[1].ToString()) - 1] == "O" &&
+                       Board[int.Parse(winCondition[2].ToString()) - 1] == "O")
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Spelare2 har vunnit!!");
+                        Console.ForegroundColor = ConsoleColor.Gray;
+
+
+
+
+                        Environment.Exit(0);
+
+
+
+                    }
+
+
+                }
+            }
+        }
+    }
+}
+
