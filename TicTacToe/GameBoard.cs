ed0487eda396f0d41ec858e9bbb56cfde65df57d
KTGCode <linde_ktg@hotmail.com>
2025-09-30 14:49:03 +0200

    Removed extra victorycheck in board class

--- TicTacToe/GameBoard.cs
+++ TicTacToe/GameBoard.cs
@@ -11,20 +11,6 @@ namespace TicTacToe
 
         public string[] Board { get; set; } = new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
-        public  bool CheckVictory()
-        {
-            Console.Clear();
-            bool row1 = Board[0] == Board[1] && Board[1] == Board[2];
-            bool row2 = Board[3] == Board[4] && Board[4] == Board[5];
-            bool row3 = Board[6] == Board[7] && Board[7] == Board[8];
-            bool col1 = Board[0] == Board[3] && Board[3] == Board[6];
-            bool col2 = Board[1] == Board[4] && Board[4] == Board[7];
-            bool col3 = Board[2] == Board[5] && Board[5] == Board[8];
-            bool diagDown = Board[0] == Board[4] && Board[4] == Board[8];
-            bool diagUp = Board[6] == Board[4] && Board[4] == Board[2];
-            return row1 || row2 || row3 || col1 || col2 || col3 || diagDown || diagUp;
-        }
-
         public void PrintBoard ()
         {
             Console.Clear();
